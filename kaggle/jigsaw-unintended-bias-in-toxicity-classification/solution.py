1dba453f12d9adf59aaf00f93106873f48c8d76e
youngrok cha <line0930@gmail.com>
2019-06-03 18:05:42 +0900

    feat: stacked GRU

--- kaggle/jigsaw-unintended-bias-in-toxicity-classification/solution.py
+++ kaggle/jigsaw-unintended-bias-in-toxicity-classification/solution.py
@@ -9,22 +9,23 @@ from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 
 input_dir = '../../data/jigsaw-unintended-bias-in-toxicity-classification/'
 output_dir = '../../data/dummy/'
+is_submit = not output_dir
 
-nrows = 10000 if output_dir else None
+nrows = None if is_submit else 100000
 data = pd.read_csv(input_dir + 'train.csv', nrows=nrows)
-training_ratio = 0.8 if output_dir else 1
-ntrains = 2 ** math.floor(math.log2(data.shape[0] * 0.8))
+training_ratio = 1 if is_submit else 0.8
+ntrains = 2 ** math.floor(math.log2(data.shape[0] * training_ratio))
 train_data = data[:ntrains]
 validation_data = data[ntrains:]
 test_data = pd.read_csv(input_dir + 'test.csv')
 # train_data.sample(frac=1)
 X_train, X_val, X_test = train_data['comment_text'], validation_data['comment_text'], test_data['comment_text']
-Y_train, Y_val = train_data['target'], validation_data['target']
+Y_train, Y_val = train_data['target'], validation_data['target'] >= 0.5
 
 vocab_size = 30000
 embedding_dim = 16
 batch_size = 512
-epoch = 5
+epoch = 3
 tokenizer = Tokenizer(num_words=vocab_size, oov_token='<UNK>')
 tokenizer.fit_on_texts(X_train)
 X_train = tokenizer.texts_to_sequences(X_train)
@@ -38,7 +39,8 @@ num_words = min(vocab_size, len(tokenizer.word_index)) + 1
 
 l = []
 l.append(layers.Embedding(num_words, embedding_dim))
-l.append(layers.GRU(16))
+l.append(layers.GRU(16, return_sequences=True))
+l.append(layers.GRU(4))
 l.append(layers.Dense(1, activation='sigmoid'))
 model = keras.Sequential(l)
 model.compile(optimizer='adam', loss='binary_crossentropy')
@@ -46,15 +48,18 @@ model.compile(optimizer='adam', loss='binary_crossentropy')
 for i in range(epoch):
     t = time.time()
     agg_loss = 0
+    print('epoch: %d' % i)
     for j in range(0, ntrains, batch_size):
         X_batch = X_train[j:j+batch_size]
         Y_batch = Y_train[j:j+batch_size]
         X_batch = pad_sequences(X_batch, maxlen=max(map(lambda x: len(x), X_batch)))
         agg_loss += model.train_on_batch(X_batch, Y_batch, )
-    y_pred_val = model.predict_proba(X_val)
-    roc_val = roc_auc_score(Y_val >= 0.5, y_pred_val)
-    print('epoch: %d' % i)
-    print('AUC_val: %.4f, Loss: %.4f, Time: %.4f' % (roc_val, agg_loss, time.time()-t))
+    if is_submit:
+        print('Loss: %.4f, Time: %.4f' % (agg_loss, time.time()-t))
+    else:
+        y_pred_val = model.predict_proba(X_val)
+        roc_val = roc_auc_score(Y_val, y_pred_val)
+        print('AUC_val: %.4f, Loss: %.4f, Time: %.4f' % (roc_val, agg_loss, time.time()-t))
 
 pred = model.predict(X_test)
 with open(output_dir + 'submission.csv', 'w') as f:
